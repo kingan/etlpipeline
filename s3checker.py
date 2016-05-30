e2887b540380a3d11436e0683b85e1fa5618ab11
kingan <kingan@tcd.ie>
2016-05-30 19:38:22 +0000

    S3 Handler

--- s3checker.py
+++ s3checker.py
@@ -3,6 +3,7 @@ import sys
 import time
 import boto
 import re
+from boto.s3.key import Key
 
 class s3checker():
     def __init__(self):
@@ -19,12 +20,21 @@ class s3checker():
         with open('/home/ubuntu/etlpipeline/logs/processLog.log','r') as f:
             return map((lambda x:x.split()[3]), filter((lambda x:'SUCCESS' in x), f.read().split('\n')[:-1]))
 
+    def processFile(self, filename):
+        k = Key(self.s3.get_bucket('kingan-test0'))
+        k.key = filename
+        k.get_contents_to_filename('/home/ubuntu/etlpipeline/data/%s'%filename)
+        return 0
+
     def mainChecker(self, s, m):
         while True:
             try:
                 for filename in s.checkBucket():
                     if(filename not in s.getSuccessList()):
-                        m.writeSuccessLog(filename)
+                        if(s.processFile(filename)==0):
+                            m.writeSuccessLog(filename)
+                        else:
+                            pass
                     else:
                         pass
                 time.sleep(2)
